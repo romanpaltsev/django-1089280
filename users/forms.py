5ba5ccd56e73698a1821649fe3ef51cff3149313
Roman Paltsev <roman.palcev@gmail.com>
2026-04-13 07:01:54 +0300

    13042026

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LoginUserForm(AuthenticationForm):
@@ -12,28 +12,44 @@ class LoginUserForm(AuthenticationForm):
         fields = ('username', 'password')
 
 
-class RegisterUserForm(forms.ModelForm):
-    username = forms.CharField(label="Логин")
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-input"}))
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={"class": "form-input"}))
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={"class": "form-input"}))
     password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={"class": "form-input"}))
 
     class Meta:
         model = get_user_model()
-        fields = ("username", "email", "first_name", "last_name", "password", "password2")
+        fields = ("username", "email", "first_name", "last_name", "password1", "password2")
         labels = {
             "email": "E-mail",
             "first_name": "Имя",
             "last_name": "Фамилия",
         }
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
-            raise forms.ValidationError("Пароли не совпадают!")
-        return cd["password"]
+        widgets = {
+            "email": forms.EmailInput(attrs={"class": "form-input"}),
+            "first_name": forms.TextInput(attrs={"class": "form-input"}),
+            "last_name": forms.TextInput(attrs={"class": "form-input"}),
+        }
 
     def clean_email(self):
         email = self.cleaned_data["email"]
         if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
+
+
+class ProfileUserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True, label="Логин", widget=forms.TextInput(attrs={"class": "form-input"}))
+    email = forms.CharField(disabled=True, label="E-mail", widget=forms.EmailInput(attrs={"class": "form-input"}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "email", "first_name", "last_name")
+        labels = {
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+        }
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-input"}),
+            "last_name": forms.TextInput(attrs={"class": "form-input"}),
+        }
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 
 from . import views
@@ -8,5 +8,10 @@ app_name = "users"
 urlpatterns = [
     path("login/", views.LoginUser.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page='users:login'), name="logout"),
-    path("register/", views.register, name="register"),
+
+    path("password-change/", PasswordChangeView.as_view(), name="password_change"),
+    path("password-change/done/", PasswordChangeDoneView.as_view(), name="password_change_done"),
+
+    path("register/", views.RegisterUser.as_view(), name="register"),
+    path("profile/", views.ProfileUser.as_view(), name="profile"),
 ]
